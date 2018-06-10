470e0fdd43744eda66fcfa74e4626c36ef1627fa
Joanne Kim <joannehyelee@gmail.com>
2018-06-10 09:52:26 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,33 @@
+// ***************************************************
+// INITIAL STARTING POINT FOR THE NODE/EXPRESS SERVER
+// ***************************************************
+
+// DEPENDENCIES
+// ===================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// EXPRESS CONFIGURATION
+// ===================================================
+var app = express();
+var PORT = process.env.PORT || 8000;
+
+// Middleware to handle data parsing
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Require models for syncing
+var db = require("./models");
+
+// ROUTER
+// ===================================================
+
+
+// SYNC SEQUELIZE MODELS & START EXPRESS APP
+// ===================================================
+db.sequelize.sync().then(function(){
+    app.listen(PORT, function(){
+        console.log("APP LISTENING ON PORT " + PORT);
+    });
+});
